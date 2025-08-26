334888aa3e80be32eedb6b40b807f01c9c553351
Roman <roman.mainn@gmail.com>
2025-08-26 22:35:51 +0500

    fix 3

--- src/scripts/withdrawNft.ts
+++ src/scripts/withdrawNft.ts
@@ -1,11 +1,19 @@
+import { address } from "@ton/ton";
+
 export async function withdrawNftItem(
   nftItemAddress: string,
   withdrawToAddress: string | undefined,
   userID: number | undefined,
   isTestnet: boolean
 ) {
+  if (!withdrawToAddress || !userID) {
+    return "";
+  }
+  const withdrawTo = address(withdrawToAddress).toString({
+    testOnly: isTestnet ? true : false,
+  });
   const res = await fetch(
-    `https://create-nft-go.onrender.com/api/nft-item/withdraw/${nftItemAddress}?withdraw-to=${withdrawToAddress}&owner-id=${userID}&is-testnet=${isTestnet}`,
+    `https://create-nft-go.onrender.com/api/nft-item/withdraw/${nftItemAddress}?withdraw-to=${withdrawTo}&owner-id=${userID}&is-testnet=${isTestnet}`,
     {
       method: "GET",
     }
